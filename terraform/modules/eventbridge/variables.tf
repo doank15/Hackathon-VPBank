974b4c658c226f915c1fde4ec25724be6812bf2d
doank15 <maidoan709@gmail.com>
2025-07-16 22:04:59 +0700

    Update variables tf

--- terraform/modules/eventbridge/variables.tf
+++ terraform/modules/eventbridge/variables.tf
@@ -1,7 +1,3 @@
-variable "name" {
-  type        = string
-  description = "EventBridge bus name"
-}
 variable "s3_bucket" {
   type        = string
   description = "S3 bucket for event logs"
--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -1,28 +1,3 @@
-variable "function_name" {
-  type        = string
-  description = "Lambda function name"
-}
-variable "role_arn" {
-  type        = string
-  description = "IAM role ARN for Lambda"
-}
-variable "handler" {
-  type        = string
-  description = "Lambda handler"
-}
-variable "runtime" {
-  type        = string
-  description = "Lambda runtime"
-}
-variable "filename" {
-  type        = string
-  description = "Path to deployment package"
-}
-variable "environment_variables" {
-  type        = map(string)
-  default     = {}
-  description = "Environment variables"
-}
 variable "s3_bucket" {
   type        = string
   description = "S3 bucket for logs or code"
